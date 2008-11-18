7113b2acdc8f16fcaa97615d155391dc66374097
James Darling <james@abscond.org>
2008-11-18 09:57:31 +0000

    make daemon

--- know.rb
+++ know.rb
@@ -1,14 +1,46 @@
 require 'net/http'
 require 'cgi'
-
-response = Net::HTTP.post_form URI.parse('http://www.text118118.com/data/livefeed_test.aspx'), {"timePerQuestion"=>"50"}
-knowledge = CGI.unescape(response.body.split('&').last.split('=').last).split("|").collect{ |t| t.split(";;") }
-
 require 'rubygems'
 require 'fastercsv'
 
-csv = FasterCSV.open('questions.csv', "a")
-knowledge.each do |question|
-  csv << question
+@pid_file = 'commonknowledge.pid'
+@csv_file = 'questions.csv'
+@interval = 60
+
+def process!
+  response = Net::HTTP.post_form URI.parse('http://www.text118118.com/data/livefeed_test.aspx'), {"timePerQuestion"=>"50"}
+  knowledge = CGI.unescape(response.body.split('&').last.split('=').last).split("|").collect{ |t| t.split(";;") }
+  csv = FasterCSV.open(@csv_file, "a")
+  knowledge.each do |question|
+    csv << question
+  end
+  csv.close
 end
-csv.close
+
+def daemonize!
+  if File.exists?(@pid_file)
+    puts 'pid already exists...start failed'
+    return
+  end
+  
+  pid = fork do
+    
+    Signal.trap 'TERM' do
+      File.delete(@pid_file)
+    end
+    
+    loop do
+      process!
+      sleep @interval
+    end
+    
+  end
+
+  File.open(@pid_file, 'w') { |file| file.write pid }
+  
+  Process.detach(pid)
+  
+  puts "Started daemon..."
+end
+
+daemonize!
